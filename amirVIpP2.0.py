0e2ee3bee52f3bd5d7d3b66a40aad68bdccbf99b
khhichour <155320663+khhichour@users.noreply.github.com>
2024-01-21 00:58:25 +0100

    Add files via upload

---
+++ amirVIpP2.0.py
@@ -0,0 +1,49 @@
+# اول تحديث لأداتي
+import marshal
+from urllib.request import urlopen
+import json
+from datetime import datetime
+try:
+    import requests
+except ImportError:
+    os.system('pip3 install requests')
+logo=("""   \x1b[1;97mdb   8b    d8  88Ydb
+  \x1b[1;92mdPYb  88b  d88  88__dP
+ dP\x1b[1;97m__Yb 88YbdP88  88YYb
+dp\x1b[1;92m   Yb 88 YY 88  88  Yb
+                 \x1b[1;97m|__________________________\x1b[1;92m
+[\x1b[1;97m*\x1b[1;92m]\x1b[1;97mfb: amir oux  \x1b[1;92m|[\x1b[1;97m*\x1b[1;92m]\x1b[1;97m@amiroux-root
+                 \x1b[1;92m|__________________________
+                 |[\x1b[1;97m*\x1b[1;92m]\x1b[1;97m Algeria tool ✓
+\x1b[1;92m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[\x1b[1;97m*\x1b[1;92m] \x1b[1;97mPaid: \x1b[1;91m2.0 [fast] 
+\x1b[1;92m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+[\x1b[1;97m*\x1b[1;92m] \x1b[1;97mEtisalat update 
+\x1b[1;92m____________________________________________
+\x1b[1;92m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+now = datetime.datetime.today()
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2024,1,1)
+if (x.strftime("%x"))>(g.strftime("%x")):
+ os.system("clear")
+ print(logo) 
+ print('\033[1;37m +213666953179\n\033[1;32mWill update in an hour 01:30  ')
+ print('\033[1;36m--------------------------------------------')
+ print('\033[1;32mTo connect via Telegram, \033[1;37mclick Share')  
+ med = input('\n\n\033[1;32mpass enter#')
+ os.system('xdg-open https://t.me/amiroudz')
+ exit()
+ open(".token.txt", "w").write(' . . . .')
+ print(x)
